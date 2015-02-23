2713173e3820b50e86ec73803bd961db1aabe7e1
Nenad Nikolic <nnikolic87@gmail.com>
2015-02-23 16:36:14 +0100

    adding pages controller

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,9 @@
 require 'garnet'
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+require 'pages_controller'
+
 module Blog
   class Application < Garnet::Application
   end
