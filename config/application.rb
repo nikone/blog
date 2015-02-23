6e13cbfc3f38dd98d147ffdc39a2e90531f0fbee
Nenad Nikolic <nnikolic87@gmail.com>
2015-02-23 21:39:27 +0100

    simple class autoload

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require 'garnet'
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 
-require 'pages_controller'
-
 module Blog
   class Application < Garnet::Application
   end
