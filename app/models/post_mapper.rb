cbf164308b5ba89722b269945d85adb632adc7b6
Nenad Nikolic <nnikolic87@gmail.com>
2015-02-24 22:44:30 +0100

    moving mapper to framework

--- app/models/post_mapper.rb
+++ app/models/post_mapper.rb
@@ -1,7 +1,4 @@
-require 'sqlite3'
-
-class PostMapper
-  @@db = SQLite3::Database.new File.join "db", "app.db"
+class PostMapper < Garnet::Mapper
   @@table_name = "posts"
   @@model = Post
   @@mappings = {
@@ -11,36 +8,7 @@ class PostMapper
     created_at: :created_at
   }
 
-  def save(post)
-    if post.id
-      @@db.execute("UPDATE posts SET header = ?, content = ?, created_at = ? WHERE id = ?", [post.header, post.content, post.created_at, post.id])
-    else
-      @@db.execute "INSERT INTO posts (header, content, created_at) VALUES (?,?,?)", [post.header, post.content, post.created_at.to_s]
-    end
-  end
-
-  def self.find(id)
-    row = @@db.execute("SELECT #{@@mappings.keys.join(',')} FROM #{@@table_name} WHERE id = ?", id).first
-    self.map_row_to_object(row)
-  end
-
-  def self.map_row_to_object(row)
-    model = @@model.new
-
-    @@mappings.each_value.with_index do |attribute, index|
-      model.send("#{attribute}=", row[index])
-    end
-    model
-  end
-
-  def self.findAll
-    data = @@db.execute "SELECT #{@@mappings.keys.join(',')} FROM #{@@table_name}"
-    data.map do |row|
-      self.map_row_to_object(row)
-    end
-  end
-
-  def self.delete(id)
-    data = @@db.execute "DELETE from posts WHERE id = ?", id
+  def created_at
+    @model.created_at.to_s
   end
 end
